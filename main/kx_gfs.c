0bd716def41b0ea0a311e5e1dfa53a981f4cdce4
yanrbts <772166784@qq.com>
2024-01-13 23:42:50 +0800

    add thread

--- main/kx_gfs.c
+++ main/kx_gfs.c
@@ -19,84 +19,115 @@
 #include <errno.h>
 #include <poll.h>
 #include <time.h>
+#include <pthread.h>
 #include "kx_config.h"
 #include "kx_gossip.h"
 #include "kx_log.h"
+#include "kx_linenoise.h"
 
+#define KX_PROMPT      "gfs>"
+char                   *kx_prompt = NULL;
+
+typedef struct clusternode {
+    char nodename[32];          /* node name */
+    cluster_addr_t self;        /* node self addr */
+    cluster_addr_t seed_node;   /* seed node addr */
+    cluster_gossip_t *gossip;   /* gossip handle */
+    pthread_t pthgossip;
+    pthread_rwlock_t rwlock;
+} clusternode;
+
+clusternode gcsnode;
 const char DATA_MESSAGE[] = "Hello World";
 
 void data_receiver(void *context, cluster_gossip_t *gossip, const uint8_t *data, size_t data_size) {
     // This function is invoked every time when a new data arrives.
-    printf("Data arrived: %s\n", data);
+    // printf("Data arrived: %s\n", data);
 }
 
-int main(int argc, char **argv) {
-    char *node_id = "0";
-    if (argc > 1) {
-        node_id = argv[1];
-    }
-    char message_with_ts[256];
-    size_t message_with_ts_size = 0;
-    char message[256];
-    sprintf(message, "[%s]: %s", node_id, DATA_MESSAGE);
+static void  
+do_completion(char const *prefix, linenoiseCompletions* lc) {
 
-    struct sockaddr_in self_in;
+}
+
+static void kx_loop() {
+    char       *line;
+    const char *file = "./history";
+    kx_prompt        = KX_PROMPT;
+
+    linenoiseHistoryLoad(file);
+    linenoiseSetCompletionCallback(do_completion);
+
+    while ((line = linenoise(kx_prompt)) != NULL) {
+        if (line[0] != '\0' && line[0] != '/') {
+        } else {
+
+        }
+        free(line);
+    }
+
+    linenoiseHistorySave(file);
+}
+
+static void *thread_start(void *arg){
+    int                 result;
+    struct sockaddr_in  self_in, seed_node_in;
+    char                message_with_ts[256];
+    size_t              message_with_ts_size = 0;
+    char                message[256];
+    cluster_socket_fd   gossip_fd;
+    int                 poll_interval = GOSSIP_TICK_INTERVAL;
+    int                 recv_result = 0;
+    int                 send_result = 0;
+    int                 poll_result = 0;
+    int                 send_data_interval = 5; // send data every 5 seconds
+    time_t              previous_data_msg_ts = time(NULL);
+
     self_in.sin_family = AF_INET;
     self_in.sin_port = 0; // pick up a random port.
     inet_aton("127.0.0.1", &self_in.sin_addr);
 
-    // Filling in the address of the current node.
-    cluster_addr_t self_addr = {
-        .addr = (const cluster_sockaddr *) &self_in,
-        .addr_len = sizeof(struct sockaddr_in)
-    };
-
-    // Create a new Pittacus descriptor instance.
-    cluster_gossip_t *gossip = cluster_gossip_create(&self_addr, &data_receiver, NULL);
-    if (gossip == NULL) {
-        log_error("Gossip initialization failed: %s\n", strerror(errno));
-        return -1;
-    }
-
-    // Connect to the active seed node.
-    struct sockaddr_in seed_node_in;
     seed_node_in.sin_family = AF_INET;
     seed_node_in.sin_port = htons(6500);
     inet_aton("127.0.0.1", &seed_node_in.sin_addr);
 
-    cluster_addr_t seed_node_addr = {
-        .addr = (const cluster_sockaddr *) &seed_node_in,
-        .addr_len = sizeof(struct sockaddr_in)
-    };
+    // Filling in the address of the current node.
+    gcsnode.self.addr = (const cluster_sockaddr *)&self_in;
+    gcsnode.self.addr_len = sizeof(struct sockaddr_in);
 
-    int join_result = cluster_gossip_join(gossip, &seed_node_addr, 1);
-    if (join_result < 0) {
-        log_error("Gossip join failed: %d\n", join_result);
-        cluster_gossip_destroy(gossip);
-        return -1;
+    gcsnode.seed_node.addr = (const cluster_sockaddr *)&seed_node_in;
+    gcsnode.seed_node.addr_len = sizeof(struct sockaddr_in);
+
+    // Create a new Pittacus descriptor instance.
+    gcsnode.gossip = cluster_gossip_create(&gcsnode.self, &data_receiver, NULL);
+    if (gcsnode.gossip == NULL) {
+        log_error("Gossip initialization failed: %s\n", strerror(errno));
+        return NULL;
+    }
+
+    result = cluster_gossip_join(gcsnode.gossip, &gcsnode.seed_node, 1);
+    if (result < 0) {
+        log_error("Gossip join failed: %d\n", result);
+        cluster_gossip_destroy(gcsnode.gossip);
+        goto err;
     }
 
     // Force Pittacus to send a Hello message.
-    if (cluster_gossip_process_send(gossip) < 0) {
+    if (cluster_gossip_process_send(gcsnode.gossip) < 0) {
         log_error("Failed to send hello message to a cluster.\n");
-        cluster_gossip_destroy(gossip);
-        return -1;
+        cluster_gossip_destroy(gcsnode.gossip);
+        goto err;
     }
 
     // Retrieve the socket descriptor.
-    cluster_socket_fd gossip_fd = cluster_gossip_socket_fd(gossip);
+    gossip_fd = cluster_gossip_socket_fd(gcsnode.gossip);
     struct pollfd gossip_poll_fd = {
         .fd = gossip_fd,
         .events = POLLIN,
         .revents = 0
     };
 
-    int poll_interval = GOSSIP_TICK_INTERVAL;
-    int recv_result = 0;
-    int send_result = 0;
-    int poll_result = 0;
-    int send_data_interval = 5; // send data every 5 seconds
-    time_t previous_data_msg_ts = time(NULL);
+    sprintf(message, "[%s]: %s", gcsnode.nodename, DATA_MESSAGE);
     while (1) {
         gossip_poll_fd.revents = 0;
 
@@ -104,28 +135,28 @@ int main(int argc, char **argv) {
         if (poll_result > 0) {
             if (gossip_poll_fd.revents & POLLERR) {
                 log_error("Gossip socket failure: %s\n", strerror(errno));
-                cluster_gossip_destroy(gossip);
-                return -1;
+                cluster_gossip_destroy(gcsnode.gossip);
+                goto err;
             } else if (gossip_poll_fd.revents & POLLIN) {
                 // Tell Pittacus to read a message from the socket.
-                recv_result = cluster_gossip_process_receive(gossip);
+                recv_result = cluster_gossip_process_receive(gcsnode.gossip);
                 if (recv_result < 0) {
                     log_error("Gossip receive failed: %d\n", recv_result);
-                    cluster_gossip_destroy(gossip);
-                    return -1;
+                    cluster_gossip_destroy(gcsnode.gossip);
+                    goto err;
                 }
             }
         } else if (poll_result < 0) {
             log_error("Poll failed: %s\n", strerror(errno));
-            cluster_gossip_destroy(gossip);
-            return -1;
+            cluster_gossip_destroy(gcsnode.gossip);
+            goto err;
         }
         // Try to trigger the Gossip tick event and recalculate
         // the poll interval.
-        poll_interval = cluster_gossip_tick(gossip);
+        poll_interval = cluster_gossip_tick(gcsnode.gossip);
         if (poll_interval < 0) {
             log_error("Gossip tick failed: %d\n", poll_interval);
-            return -1;
+            goto err;
         }
         // Send some data periodically.
         time_t current_time = time(NULL);
@@ -136,17 +167,35 @@ int main(int argc, char **argv) {
                                             "%s (ts = %ld)", 
                                             message, current_time) + 1;
             // message_with_ts_size = sprintf(message_with_ts, "%s (ts = %ld)", message, current_time) + 1;
-            cluster_gossip_send_data(gossip, (const uint8_t *) message_with_ts, message_with_ts_size);
+            cluster_gossip_send_data(gcsnode.gossip, (const uint8_t *) message_with_ts, message_with_ts_size);
         }
         // Tell Pittacus to write existing messages to the socket.
-        send_result = cluster_gossip_process_send(gossip);
+        send_result = cluster_gossip_process_send(gcsnode.gossip);
         if (send_result < 0) {
             log_error("Gossip send failed: %d, %s\n", send_result, strerror(errno));
-            cluster_gossip_destroy(gossip);
-            return -1;
+            cluster_gossip_destroy(gcsnode.gossip);
+            goto err;
         }
     }
-    cluster_gossip_destroy(gossip);
+    cluster_gossip_destroy(gcsnode.gossip);
+err:
+    return NULL;
+}
 
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        strncpy(gcsnode.nodename, argv[1], sizeof(gcsnode.nodename)-1);
+        gcsnode.nodename[sizeof(gcsnode.nodename)-1] = '\0';
+    }
+
+    if (pthread_rwlock_init(&gcsnode.rwlock, NULL) != 0) {
+        return -1;
+    }
+    
+    pthread_create(&gcsnode.pthgossip, NULL, thread_start, NULL);
+
+    kx_loop();
+
+    pthread_rwlock_destroy(&gcsnode.rwlock);
     return 0;
 }
